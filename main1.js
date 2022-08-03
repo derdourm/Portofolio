24b4bcc9f4134e16e45c22947824ad01fee52a91
unknown <Derdour Mohamed>
2022-08-03 23:17:03 +0100

    back to normale mode

--- main1.js
+++ main1.js
@@ -1,7 +1,8 @@
 function darkmode() {
-  document.body.style.background = "#030508";  
-  document.getElementById("para").style.color ="white"
+  document.body.style.background = "#030508";
+  document.getElementById("para").style.color = "white";
 }
-function white() {
-
+function whitemode() {
+  document.body.style.background = "#ffffff";
+  document.getElementById("para").style.color = "black";
 }
